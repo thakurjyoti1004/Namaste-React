be1a70d64b865ef9886d2b0f1161c8d3e1083502
jyoti <jthakur777.jt@gmail.com>
2024-01-11 08:22:12 -0800

    adds heading function component ep-2

--- App.js
+++ App.js
@@ -1,3 +1,59 @@
-const heading = React.createElement("h1",{id:"heading"},"Hello From React!");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+// const heading =React.createElement("div",{className:"title"},null,[React.createElement("h1",null,"Heading 1"),React.createElement("h2",null,"Heading 2"),React.createElement("h3",null,"Heading 3")])
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+
+// const heading = <div className="title">
+//     <h1>Heading 1</h1>
+//     <h2>heading 2</h2>
+//     <h3>heading 3</h3>
+//     </div>;
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+
+// const HeadingFunction = () =>{
+
+//     return(<div className="title">
+//             <h1>heading 1</h1>
+//             <h2>heading 2</h2>
+//             <h3>heading 3</h3>
+//             </div>
+//            )
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<HeadingFunction />);
+
+
+
+// const element = "is here!";
+
+// const Title = () =>{
+//     return <h1>Title: Namaste React!</h1>
+// }
+
+// const HeadingFunction = () =>{
+//     return(<div className="title">
+//             <Title />
+//             <h1>heading 1 {element}</h1>
+//             <h2>heading 2 {element}</h2>
+//             <h3>heading 3 {element}</h3>
+//             </div>
+//            )
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<HeadingFunction />);
+
+
+
+const Header = () =>{
+    return (
+        
+    )
+}
