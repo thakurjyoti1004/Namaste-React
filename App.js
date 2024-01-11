7b681524a495a825c5ec9bce4e3a632e8993cc9b
jyoti <jthakur777.jt@gmail.com>
2024-01-11 08:04:13 -0800

    adds header functional component

--- App.js
+++ App.js
@@ -2,6 +2,58 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const heading = React.createElement("div",{id:"parent"},"This is the parent div",[React.createElement("div",{id:"child-1"},"Child div 1"),[React.createElement("h1",{id:"header"},"Heading 1"), React.createElement("h2",{id:"header"},"Heading 2")]])
-const rootElement = ReactDOM.createRoot(document.getElementById("root"));
-rootElement.render(heading);
+// const heading =React.createElement("div",{className:"title"},null,[React.createElement("h1",null,"Heading 1"),React.createElement("h2",null,"Heading 2"),React.createElement("h3",null,"Heading 3")])
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+
+// const heading = <div className="title">
+//     <h1>Heading 1</h1>
+//     <h2>heading 2</h2>
+//     <h3>heading 3</h3>
+//     </div>;
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+
+// const HeadingFunction = () =>{
+
+//     return(<div className="title">
+//             <h1>heading 1</h1>
+//             <h2>heading 2</h2>
+//             <h3>heading 3</h3>
+//             </div>
+//            )
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<HeadingFunction />);
+
+
+
+// const element = "is here!";
+
+// const Title = () =>{
+//     return <h1>Title: Namaste React!</h1>
+// }
+
+// const HeadingFunction = () =>{
+//     return(<div className="title">
+//             <Title />
+//             <h1>heading 1 {element}</h1>
+//             <h2>heading 2 {element}</h2>
+//             <h3>heading 3 {element}</h3>
+//             </div>
+//            )
+// }
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<HeadingFunction />);
+
+
+
+const Header = () =>{
+    return (
+        
+    )
+}
