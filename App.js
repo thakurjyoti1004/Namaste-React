1b1580f9ad2473d44c0592f77fd8b82affd339cb
jyoti <jthakur777.jt@gmail.com>
2024-01-13 21:30:38 -0800

    started building food ordering app

--- App.js
+++ App.js
@@ -1,59 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import Logo from './logos/fooodLogo.png';
 
-// const heading =React.createElement("div",{className:"title"},null,[React.createElement("h1",null,"Heading 1"),React.createElement("h2",null,"Heading 2"),React.createElement("h3",null,"Heading 3")])
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img src={Logo} alt="logo"/>
+      </div>
+    </div>
+  );
+};
 
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
 
-// const heading = <div className="title">
-//     <h1>Heading 1</h1>
-//     <h2>heading 2</h2>
-//     <h3>heading 3</h3>
-//     </div>;
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-
-
-// const HeadingFunction = () =>{
-
-//     return(<div className="title">
-//             <h1>heading 1</h1>
-//             <h2>heading 2</h2>
-//             <h3>heading 3</h3>
-//             </div>
-//            )
-// }
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<HeadingFunction />);
-
-
-
-// const element = "is here!";
-
-// const Title = () =>{
-//     return <h1>Title: Namaste React!</h1>
-// }
-
-// const HeadingFunction = () =>{
-//     return(<div className="title">
-//             <Title />
-//             <h1>heading 1 {element}</h1>
-//             <h2>heading 2 {element}</h2>
-//             <h3>heading 3 {element}</h3>
-//             </div>
-//            )
-// }
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<HeadingFunction />);
-
-
-
-const Header = () =>{
-    return (
-        
-    )
-}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
