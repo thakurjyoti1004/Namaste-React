a3e4404e3898c5a1eb0f86dca7f544f868a5bd89
jyoti <jthakur777.jt@gmail.com>
2024-01-17 22:12:03 +0530

    created restaurant cards

--- App.js
+++ App.js
@@ -1,13 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import Logo from './logos/fooodLogo.png';
+import Logo from "./logos/fooodLogo.png";
 
 const Header = () => {
   return (
     <div className="header">
-      <div>
-        <img src={Logo} alt="logo"/>
+      <div className="logo-container">
+        <img className="logo" src={Logo} alt="logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = (props) => {
+  const { resName, cuisine } = props;
+  return (
+    <div className="restaurant-card">
+      <img
+        className="restaurant-image"
+        src="https://d4t7t8y8xqo0t.cloudfront.net/resized/720X480/group%2F1542%2Fmenu020170930184033.jpg"
+        alt="res-img"
+      />
+      <h3>{resName}</h3>
+      <h4>{cuisine}</h4>
+      <h4>4.5 Stars</h4>
+      <h4>38 min.</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body-container">
+      <div className="search-bar">Search bar</div>
+      <div className="restaurant-container">
+        <RestaurantCard
+          resName="Meghna foods"
+          cuisine="Biryani, North Indian, Asian"
+        />
+        <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" />
       </div>
     </div>
   );
@@ -17,6 +56,7 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
