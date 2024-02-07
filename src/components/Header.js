79e4cc084ee07358f57edc5cbf906d39e28be27d
jyoti <jthakur777.jt@gmail.com>
2024-02-07 18:03:57 +0530

    login & logout btn added to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,12 @@
+import { useState } from "react";
 import Logo from "../../logos/fooodLogo.png";
 const Header = () => {
+  const [loginBtn, setLoginBtn] = useState("Login");
+
+  const handleLoginBtn = () => {
+    loginBtn === "Login" ? setLoginBtn("Logout") : setLoginBtn("Login");
+  };
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -11,6 +18,9 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button className="login-btn" onClick={handleLoginBtn}>
+            {loginBtn}
+          </button>
         </ul>
       </div>
     </div>
