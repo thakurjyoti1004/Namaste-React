55a73037b9b8daa25fb500ba014005ff35568357
jyoti <jthakur777.jt@gmail.com>
2024-02-23 23:06:32 +0530

    state updated in class component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,14 +4,21 @@ class UserClass extends React.Component {
     super(props);
     this.state = {
       count: 0,
-      count2: 2,
     };
   }
   render() {
     return (
       <div className="user-card">
         <h1>Count:{this.state.count}</h1>
-        <h1>Count2:{this.state.count2}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Add Count
+        </button>
         <h1>Name:{this.props.name}(Class)</h1>
         <h2>Location:{this.props.location}</h2>
         <h3>jthakur777.jt@gmail.com</h3>
